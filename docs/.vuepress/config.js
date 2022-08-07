85225f904de5e6c9a60b4c7ef2b602c18623368c
HTuoZhou <1613710573@qq.com>
2022-08-07 16:26:20 +0800

    :tada: 基础搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,73 @@
+module.exports = {
+    locales:{
+        '/': {
+            lang: 'zh-CN',
+            title: 'HTuoZhouReviewGuide',
+            description: 'HTuoZhouReviewGuide'
+        },
+    },
+    base: '/HTuoZhouReviewGuide/',
+    head: [
+        ["link", {rel: "icon", href: "/assets/img/logo.png"}]
+    ],
+    plugins: [
+        [
+            '@vuepress/last-updated',
+            {
+                transformer: (timestamp, lang) => {
+                    // 不要忘了安装 moment
+                    const moment = require('moment')
+                    moment.locale(lang)
+                    return moment(new Date(timestamp)).utcOffset(8).format('YYYY-MM-DD HH:mm:ss')
+                }
+            }
+        ],
+        ['@vuepress/back-to-top'],
+        ['@vuepress/nprogress'],
+        ['@vuepress/active-header-links']
+    ],
+    themeConfig: {
+        repo: 'HTuoZhou/HTuoZhouReviewGuide/',
+        repoLabel: '查看源码',
+        logo: '/assets/img/logo.png',
+        sidebarDepth: 2,
+        smoothScroll: true,
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '数据结构与算法',
+                ariaLabel: '数据结构与算法',
+                items: [
+                    { text: '数据结构', link: '/dataStruct-algorithm/dataStruct/' },
+                    { text: '算法', link: '/dataStruct-algorithm/algorithm/' }
+                ]},
+            { text: '设计模式', link: '/design-pattern/' },
+            { text: '呱哇', link: '/java/' },
+            { text: '呱哇虚拟机', link: '/jvm/' },
+            {
+                text: '微服务',
+                ariaLabel: '微服务',
+                items: [
+                    { text: 'SpringBoot', link: '/micro-service/spring-boot/' },
+                    { text: 'SpringCloud', link: '/micro-service/spring-cloud/' },
+                    { text: 'SpringCloudAlibaba', link: '/micro-service/spring-cloud-alibaba/' }
+                ]
+            },
+            { text: '系统设计', link: '/system-design/' }
+        ],
+        sidebar: {
+            '/dataStruct-algorithm/dataStruct/': [''],
+            '/dataStruct-algorithm/algorithm/': [''],
+            '/design-pattern/': [''],
+            '/java/': [''],
+            '/jvm/': [''],
+            '/micro-service/spring-boot/': [''],
+            '/micro-service/spring-cloud/': [''],
+            '/micro-service/spring-cloud-alibaba/': [''],
+            '/system-design/': ['']
+        },
+        lastUpdated: '上一次更新时间',
+        editLinks: true,
+        editLinkText: '帮助我们改善此页面！'
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,11 @@
+---
+home: true
+heroImage: /hero.png
+heroText: HTuoZhouReviewGuide
+actionText: Fast Begin →
+actionLink: /dataStruct-algorithm/dataStruct/
+features:
+- title: Personal Introduce
+  details: My name is HTuoZhou,I was a programmer in 1997
+- title: Personal Motto
+  details: The beginning of a day is in the morning,A year's plan begins in spring,The best thing in life is diligence
